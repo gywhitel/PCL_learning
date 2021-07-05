663a4f255af4ac6b1877f4f4008204e6a85d7e0a
yinghao <gywhitel@foxmail.com>
2021-07-05 20:57:14 +0800

    reorganize the repo

---
+++ IO/read_pcd/build/CMakeFiles/read_pcd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../read_pcd"
+  "../read_pcd.pdb"
+  "CMakeFiles/read_pcd.dir/read_pcd.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/read_pcd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ IO/write_pcd/build/CMakeFiles/pcd_write.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../pcd_write"
+  "../pcd_write.pdb"
+  "CMakeFiles/pcd_write.dir/pcd_write.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcd_write.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
